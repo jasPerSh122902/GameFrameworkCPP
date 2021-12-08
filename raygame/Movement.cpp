b3dba057e12b94e178b10efb2f910360d92f0cc5
jasper <s218005@students.aie.edu>
2021-12-08 16:52:22 -0600

    the movement is not done i need to get  that finished.

--- raygame/Movement.cpp
+++ raygame/Movement.cpp
@@ -1,1 +1,22 @@
 #include "Movement.h"
+
+
+void Movement::Update(float deltaTime)
+{
+	 MathLibrary::Vector2 moveDirection = MathLibrary::Vector2(Directionx, Directiony);
+
+	//caculates the veclocity 
+	m_velocity = moveDirection.normalize* speed * deltaTime;
+	//addes the velocity to the localPosistion
+	LocalPosistion += m_velocity;
+
+}
+
+/// <summary>
+/// Returns the new Vector3 for the localPosistion and sets the Translation with the values of x, y.
+/// </summary>
+MathLibrary::Vector2 LocalPosistion
+{
+    //takes in a posisition on the matrix...
+     return MathLibrary::Vector2()
+}
