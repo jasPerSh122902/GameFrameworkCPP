6e1ce93e869d76a935e011b7327324942d4f94f8
jasper <s218005@students.aie.edu>
2022-02-23 16:37:25 -0600

    commiting out the sprite componet in the player class

--- raygame/Player.cpp
+++ raygame/Player.cpp
@@ -15,7 +15,7 @@ void Player::start()
 	m_inputComponet = dynamic_cast<Input*>(addComponent(new Input()));
 	m_movementComponet = dynamic_cast<Movement*>(addComponent(new Movement()));
 	m_movementComponet->setMaxSpeed(500);
-	m_spriteComponet = dynamic_cast<Sprite*>(addComponent(new Sprite("Sprite/Learth.png")));
+	//m_spriteComponet = dynamic_cast<Sprite*>(addComponent(new Sprite("Sprite/Learth.png")));
 }
 
 void Player::update(float deltaTime)
